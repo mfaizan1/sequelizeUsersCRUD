d7d69295c4aaf31a1a20d80a9ed2e69215d4bdd4
mfaizan1 <mfaizan1ulhaq1@gmail.com>
2019-11-29 18:30:38 -0800

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const config = require('./config/serverConfig.json');
 app.use(bodyParser.json());
 require('./routes')(app);
