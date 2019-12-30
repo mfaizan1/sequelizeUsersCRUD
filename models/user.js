dde3e60c1bba2f9dc5193172a5325ecc24d4a297
mfaizan1 <mfaizan1ulhaq1@gmail.com>
2019-12-30 15:48:27 -0800

    improved user table

--- models/user.js
+++ models/user.js
@@ -1,14 +1,56 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    profilePicture: DataTypes.STRING,
-    displayName: DataTypes.STRING,
-    
-  }, {});
+    id:{
+      allowNull: false,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    sortId:{
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+     type: DataTypes.STRING,
+     unique: false,
+     allowNull:false,
+     validate:{
+      len:[1,50]
+     }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      unique: false,
+      allowNull:false,
+      validate:{
+       len:[1,50]
+      }
+    },
+    email:{
+     type: DataTypes.STRING,
+     unique: true,
+     allowNull:false,
+     validate:{
+      len:[6,100],
+      isEmail: true,
+     }
+    } ,
+    password: {
+     type: DataTypes.STRING,
+     allowNull:true,
+     validate:{
+        notEmpty:true,
+        len:[8,300]
+     }
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+      unique: false,
+      allowNull:false
+      }
+}, {});
   User.associate = function(models) {
     // associations can be defined here
   };
